ec88de48e964f604c776eef0b301efaeb5393575
Imran Hasan <imran.h94@gmail.com>
2024-03-18 22:35:14 +0500

    added styling to numberinput field and restricted length to 2

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +26,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 }, //specific to iOS
     shadowRadius: 6, //specific to iOS
     shadowOpacity: 0.25, //specific to iOS
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
