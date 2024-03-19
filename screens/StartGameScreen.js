8e4498a01a0d6b9a37117e887a5553abb8f9a335
Imran Hasan <imran.h94@gmail.com>
2024-03-20 00:47:19 +0500

    installed expo-linear-gradient with npx expo and added it to rootScreen styles

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4, //specific to android to add shadow
     shadowColor: "black", //specific to iOS
