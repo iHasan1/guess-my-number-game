45bd096373aa8677adde57031a7263f6a92f7fd9
Imran Hasan <imran.h94@gmail.com>
2024-03-20 01:19:26 +0500

    added logic to change screen programmatically

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 import { useState } from "react";
 
-function StartGameScreen() {
+function StartGameScreen({ onPickedNumber }) {
   const [enteredNumber, setEnteredNumber] = useState("");
 
   function numberInputHandler(enteredText) {
@@ -25,7 +25,7 @@ function StartGameScreen() {
       return;
     }
 
-    console.log("Valid Number");
+    onPickedNumber(chosenNumber);
   }
 
   return (
